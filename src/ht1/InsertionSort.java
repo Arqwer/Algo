c8cbd139541b92d2166f91b35081cbfb00ccfce5
Roman <shedr@smtp.ru>
2012-10-01 19:08:00 +0400

    shift

--- src/ht1/InsertionSort.java
+++ src/ht1/InsertionSort.java
@@ -12,7 +12,7 @@ public class InsertionSort {
   public static long[] mass = new long[M_SIZE];
 
   public static void straitIns(long[] m, int sorted) {
-    long tmp, last;
+    long last;
     last = m[sorted + 1];
     for (int i = sorted; i >= 0; i--) {
       if (last < m[i]) {
@@ -23,7 +23,7 @@ public class InsertionSort {
   }
 
   public static void straitBinIns(long[] m, int sorted) {
-    System.out.println("SBI entered");                 //mbd
+    // System.out.println("SBI entered");                 //mbd
     System.out.println("   m:" + Arrays.toString(m) + "s:" + sorted);
     int k;
     long last;
@@ -33,27 +33,31 @@ public class InsertionSort {
       mShift(m, k);
       m[k] = last;
     }
-    System.out.println("SBI closed");    //mbd
+    //  System.out.println("SBI closed");    //mbd
   }
 
   public static void mShift(long[] m, int from) {
-    long tmp;
-    for (int i = from; i < m.length - 1; i++) {
-      tmp = m[i + 1];
+
+    for (int i = m.length - 2; i >= from; i--) {
       m[i + 1] = m[i];
-      m[i] = tmp;
+      System.out.println(Arrays.toString(m));
     }
   }
 
   public static int binSearch(long[] m, long val, int from, int to) {
+    /*  System.out.println("BS entered");                 //mbd
+System.out.println("   m:" + Arrays.toString(m)+"\nv:"+val+"f:"+from+"t:"+to); */
     int mid;
-    for (mid = (from + to) / 2; (from < to) && (m[mid] != val); ) {
+    for (mid = (from + to) / 2; to - from > 1; ) {
       mid = (from + to) / 2;
       if (m[mid] < val) {
         from = mid;
       } else to = mid;
-
+      if (m[mid] == val) {
+        break;
+      }
     }
+    //System.out.println("BS closed");
     return mid;
   }
 
@@ -74,8 +78,11 @@ public class InsertionSort {
     for (int i = 0; i < M_SIZE; i++) {
       mass[i] = random.nextInt(MAX_RAND + 1);
     }
-    System.out.println("k" + binSearch(mass, 6, 0, 9));
-    //insertionBinSort(mass);
+    //insertionSort(mass);
+    // System.out.println("k" + binSearch(mass, 6, 0, 9));
+    insertionBinSort(mass);
     System.out.println(Arrays.toString(mass));
+    //mShift(mass, 3);
+    //System.out.println(Arrays.toString(mass));
   }
 }
