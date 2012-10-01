b78d79da56d39306da482b36162ae5ce7128da90
Roman <shedr@smtp.ru>
2012-10-01 18:43:14 +0400

    Bin

--- src/ht1/InsertionSort.java
+++ src/ht1/InsertionSort.java
@@ -8,13 +8,13 @@ import java.util.Random;
  * Date: 01.10.12
  */
 public class InsertionSort {
-  public static final int M_SIZE = 100;
+  public static final int M_SIZE = 10, MAX_RAND = 10;
   public static long[] mass = new long[M_SIZE];
 
   public static void straitIns(long[] m, int sorted) {
     long tmp, last;
     last = m[sorted + 1];
-    for (int i = sorted; i > 0; i--) {
+    for (int i = sorted; i >= 0; i--) {
       if (last < m[i]) {
         m[i + 1] = m[i];
         m[i] = last;
@@ -22,18 +22,60 @@ public class InsertionSort {
     }
   }
 
+  public static void straitBinIns(long[] m, int sorted) {
+    System.out.println("SBI entered");                 //mbd
+    System.out.println("   m:" + Arrays.toString(m) + "s:" + sorted);
+    int k;
+    long last;
+    last = m[sorted + 1];
+    if (last < m[sorted]) {
+      k = binSearch(m, m[sorted + 1], 0, sorted);
+      mShift(m, k);
+      m[k] = last;
+    }
+    System.out.println("SBI closed");    //mbd
+  }
+
+  public static void mShift(long[] m, int from) {
+    long tmp;
+    for (int i = from; i < m.length - 1; i++) {
+      tmp = m[i + 1];
+      m[i + 1] = m[i];
+      m[i] = tmp;
+    }
+  }
+
+  public static int binSearch(long[] m, long val, int from, int to) {
+    int mid;
+    for (mid = (from + to) / 2; (from < to) && (m[mid] != val); ) {
+      mid = (from + to) / 2;
+      if (m[mid] < val) {
+        from = mid;
+      } else to = mid;
+
+    }
+    return mid;
+  }
+
   public static void insertionSort(long[] m) {
-    for (int i = 0; i < M_SIZE; i++) {
+    for (int i = 0; i < M_SIZE - 1; i++) {
       straitIns(m, i);
+    }
+  }
+
+  public static void insertionBinSort(long[] m) {
+    for (int i = 0; i < M_SIZE - 1; i++) {
+      straitBinIns(m, i);
     }
   }
 
   public static void main(String[] args) {
     Random random = new Random();
     for (int i = 0; i < M_SIZE; i++) {
-      mass[i] = random.nextInt();
+      mass[i] = random.nextInt(MAX_RAND + 1);
     }
-    insertionSort(mass);
+    System.out.println("k" + binSearch(mass, 6, 0, 9));
+    //insertionBinSort(mass);
     System.out.println(Arrays.toString(mass));
   }
 }
