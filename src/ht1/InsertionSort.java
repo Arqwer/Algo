51bdd4a0eaaf0934bc0f1f2f1beb0adb0d0ed031
Roman <shedr@smtp.ru>
2012-10-01 17:18:52 +0400

    StraitIns

---
+++ src/ht1/InsertionSort.java
@@ -0,0 +1,27 @@
+package ht1;
+
+/**
+ * User: Roman
+ * Date: 01.10.12
+ */
+public class InsertionSort {
+  public static final int M_SIZE = 100;
+  public static long[] mass = new long[M_SIZE];
+
+  public static void straitIns(long[] m, int sorted) {
+    long tmp, last;
+    last = m[sorted + 1];
+    for (int i = sorted; i > 0; i--) {
+      if (last > m[i]) {
+        m[i + 1] = m[i];
+        m[i] = last;
+      } else break;
+    }
+  }
+
+  public static void main(String[] args) {
+    for (int i = 0; i < 10; i++) {
+      mass[i] = i;
+    }
+  }
+}
