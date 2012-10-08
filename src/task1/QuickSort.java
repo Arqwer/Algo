e0d07814e985e3e253dc3227412cfbc2e6daa077
Roman <shedr@smtp.ru>
2012-10-08 20:14:13 +0400

    quick+ins

--- src/task1/QuickSort.java
+++ src/task1/QuickSort.java
@@ -13,32 +13,46 @@ public class QuickSort {
   public static final int M_SIZE = 10;
   public static final int MAX_RAND = 20;
 
-  public static void quickSort(long[] arr, int low, int high) {
-    int i = low;
-    int j = high;
-    long x = arr[low];
-    do {
-      while (arr[i] < x) {
-        i++;
+  public static void insertionSort(long[] m, int low, int high) {
+    if (low < high) {
+      for (int i = low; i < high; i++) {
+        long last;
+        last = m[i + 1];
+        for (int j = i; j >= low; j--) {
+          if (last < m[j]) {
+            m[j + 1] = m[j];
+            m[j] = last;
+          } else break;
+        }
       }
-      while (arr[j] > x) {
-        j--;
-      }
-      if (i <= j) {
-        long temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-        i++;
-        j--;
-      }
-    } while (i <= j);
-    if (low < j) {
-      quickSort(arr, low, j);
-    }
-    if (i < high) {
-      quickSort(arr, i, high);
     }
   }
+
+  public static void quickSort(long[] arr, int low, int high) {
+    if (high - low > 4) {
+      int i = low;
+      int j = high;
+      long bar = arr[low];
+      do {
+        while (arr[i] < bar) {
+          i++;
+        }
+        while (arr[j] > bar) {
+          j--;
+        }
+        if (i <= j) {
+          long temp = arr[i];
+          arr[i] = arr[j];
+          arr[j] = temp;
+          i++;
+          j--;
+        }
+      } while (i <= j);
+      quickSort(arr, low, j);
+      quickSort(arr, i, high);
+    } else insertionSort(arr, low, high);
+  }
+
 
   public static void main(String[] args) {
     long[] arr = new long[M_SIZE];
