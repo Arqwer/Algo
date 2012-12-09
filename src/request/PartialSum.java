0c85ebe8dcea16781dca1c149e4d933c85e66bae
Roman <shedr@smtp.ru>
2012-12-09 15:29:15 +0400

    added segment tree but doesn't work

--- src/request/PartialSum.java
+++ src/request/PartialSum.java
@@ -10,6 +10,7 @@ import java.util.Locale;
 public class PartialSum {
   static int[] values;
   public static int[] psum;
+  public static int[] tSumArr;
 
   private static void solve() throws IOException {
     final int n = rInt();
@@ -20,13 +21,14 @@ public class PartialSum {
       values[i] = rInt();
     }
     init();
+    tInit();
     for (int i = 0; i < m; i++) {
       final char c = rNext().charAt(0);
       if (c == 'S') {
-        out.println(sum(rInt(), rInt()));
-        System.out.println(sum(rInt(), rInt()));
+//        out.println(sum(rInt(), rInt()));
+        System.out.println(tSum(0, 0, tSumArr.length, rInt(), rInt()));
       } else if (c == 'U') {
-        update(rInt(), rInt());
+        tUpdate(0, 0, tSumArr.length, rInt(), rInt());
       }
     }
   }
@@ -40,8 +42,52 @@ public class PartialSum {
     }
   }
 
+  private static void tInit() {
+    tSumArr = new int[4 * values.length];
+    build(0, 0, tSumArr.length - 1);
+  }
+
+  private static void build(int v, int l, int r) {
+    if (r == l) {
+      tSumArr[v] = values[l];
+    } else {
+      build(2 * v, l, (l + r) / 2);
+      build(2 * v + 1, (l + r) / 2 + 1, r);
+      tSumArr[v] = tSumArr[2 * v] + tSumArr[2 * v + 1];
+    }
+  }
+
+  private static int tSum(int v, int tl, int tr, int l, int r) {
+    if (l > r) {
+      return 0;
+    }
+    if ((l == tl) && (r == tr)) {
+      return tSumArr[v];
+    }
+    int tm = (tl + tr) / 2;
+    return tSum(2 * v, tl, tm, l, r) + tSum(2 * v + 1, tm + 1, tr, l, r);
+  }
+
+  private static void tUpdate(int v, int tl, int tr, int pos, int new_val) {
+    if (tl == tr) {
+      tSumArr[v] = new_val;
+    } else {
+      int tm = (tl + tr) / 2;
+      if (pos <= tm)
+        tUpdate(v * 2, tl, tm, pos, new_val);
+      else
+        tUpdate(v * 2 + 1, tm + 1, tr, pos, new_val);
+      tSumArr[v] = tSumArr[v * 2] + tSumArr[v * 2 + 1];
+    }
+  }
+
   private static void update(int pos, int val) {
-    // TODO: implement
+    values[pos] = val;
+    int sum = 0;
+    for (int i = 1; i <= values.length; i++) {
+      sum += values[i - 1];
+      psum[i] = sum;
+    }
   }
 
   private static int sum(int i, int j) {
